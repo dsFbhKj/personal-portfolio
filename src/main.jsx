321f2584b1ae077f7a636fa9daef64fa133c1176
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-03-12 01:31:00 -0700

    config tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import './styles/index.css';
+import './styles/global.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import  Experience  from './components/Experience';
+import Experience from './components/Experience';
 import Main from './pages/Main';
 import World from './pages/World';
 import Hyperspace from './components/Hyperspace';
 
 const router = createBrowserRouter([
-  { path: '/', element: <Experience /> },
+  { path: '/experience', element: <Experience /> },
+  { path: '/', element: <Main /> },
   { path: '/hyperspace', element: <Hyperspace /> },
   { path: '/world', element: <World /> },
-  { path: '/main', element: <Main /> },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import { useSpring, animated } from '@react-spring/web'; //imported animation for react-spring
 import AOS from 'aos'; //need both the import
 import 'aos/dist/aos.css'; //and css for AOS to work
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
 export default function Main() {
   const [springs, api] = useSpring(() => ({
@@ -24,8 +24,21 @@ export default function Main() {
   };
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 polygons'>
-      {/* Add react Soring Animation */}
+    <>
+      {/* Tested AOS Div animation wrapping it in daisy button */}
+      {/* Tested out a button using daisy ui(tailwind) */}
+      <button class='inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900'>
+        Button
+      </button>
+      <button
+        data-aos='fade-up-right'
+        data-aos-duration='3000'
+        className='inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900'
+      >
+        Button
+      </button>
+
+      {/* Add react spring Animation */}
       <animated.div
         onClick={handleClick}
         style={{
@@ -36,6 +49,6 @@ export default function Main() {
           ...springs,
         }}
       />
-    </main>
+    </>
   );
 }
