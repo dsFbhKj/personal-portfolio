75804fd03b34e5015cf81af39c23ecc584209bfe
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-03-12 00:27:46 -0700

    added pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { Experience } from './components/Experience';
+import  Experience  from './components/Experience';
+import Main from './pages/Main';
+import World from './pages/World';
+import Hyperspace from './components/Hyperspace';
 
 const router = createBrowserRouter([
   { path: '/', element: <Experience /> },
-  // { path: '/hyperspace', element: <Hyperspace /> },
-  // { path: '/world', element: <World /> },
-  // { path: '/main', element: <Main /> },
+  { path: '/hyperspace', element: <Hyperspace /> },
+  { path: '/world', element: <World /> },
+  { path: '/main', element: <Main /> },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
