0c13c6545fd93999e16ca53fc98e1f99b1c2eb11
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-03-26 15:31:51 -0700

    added react icons

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { FaLinkedin, FaGithub, FaYoutube } from 'react-icons/fa';
 const Landing = () => {
   return (
     <>
@@ -7,9 +7,33 @@ const Landing = () => {
     <div className="hero-overlay bg-opacity-60"></div>
     <div className="hero-content text-center text-neutral-content">
       <div className="max-w-md">
-        <h1 className="mb-5 text-5xl font-bold">Georgiana Barefield</h1>
-        <p className="mb-5">Hi there! Welcome to my portfolio, where creativity meets development. To get started on your journey of imagination and innovation, simply click the button below. Let's explore together!</p>
-        <button className="btn btn-neutral"><a href='/experience'>Begin</a></button>
+        <h1 className="mb-7 text-7xl font-bold">Georgiana Barefield</h1>
+        {/* <p className="mb-7">Hi there! Welcome to my portfolio, where creativity meets development. To get started on your journey of imagination and innovation, simply click the button below. Let's explore together!</p> */}
+        <p className="mb-5">Want to learn more?</p>
+        <div className='quick-links'>
+          <a
+            href='https://www.linkedin.com/in/georgibarefield'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaLinkedin size='2.5rem' color='#3a77cc' />
+          </a>
+          <a
+            href='https://github.com/gbaref949'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaGithub size='2.5rem' color='black' />
+          </a>
+          <a
+            href='https://www.youtube.com/@georgiB1'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaYoutube size='2.5rem' color='red' />
+          </a>
+          </div>
+        <button className="btn btn-neutral"><a href='/main'>Begin</a></button>
       </div>
     </div>
   </div>
