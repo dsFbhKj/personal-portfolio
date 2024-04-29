56d65ae033605a98aa91e0e2b1758265e0c03b47
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-04-29 04:33:06 -0700

    fixed landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -36,18 +36,17 @@ const Landing = () => {
           <div className='max-w-md'>
             <div onClick={() => set((state) => !state)}>
               <Trail open={open}>
-                <span>Georgiana</span>
-                <span>Barefield</span>
+                <span className='whitespace-nowrap'>Georgiana Barefield</span>
               </Trail>
             </div>
             {/* <h1 className='mb-7 text-7xl font-bold flex items-stretch whitespace-nowrap justify-center'>Georgiana Barefield</h1> */}
-            <p className='mb-7'>
+            <p className='mb-10 mt-5'>
               Hi there! Welcome to my portfolio, where creativity meets
               development. To get started on your journey of imagination and
               innovation, simply click the button below. Let's explore together!
             </p>
             <div className='flex justify-start items-start'>
-              <div className='card w-1/3 bg-base-100 shadow-xl mr-5'>
+              <div className='card w-2/4 bg-base-100 shadow-xl mr-10 ml-4'>
                 <figure>
                   <video
                     src={me}
@@ -59,11 +58,11 @@ const Landing = () => {
                   />
                 </figure>
               </div>
-              <div className='flex flex-col items-end'>
+              <div className='flex flex-col justify-around mt-10'>
                 <p>
                   <strong>Want to learn more?</strong>
                 </p>
-                <div className='flex items-center space-x-4 mb-5 mt-5'>
+                <div className='flex justify-center space-x-4 mb-5 mt-5'>
                   <a
                     href='https://www.linkedin.com/in/georgibarefield'
                     target='_blank'
@@ -89,7 +88,7 @@ const Landing = () => {
                 <a
                   role='button'
                   href='/world'
-                  className='btn btn-neutral btn-active bg-pink-500'
+                  className='btn btn-neutral btn-active bg-pink-500 button'
                 >
                   Begin
                 </a>
